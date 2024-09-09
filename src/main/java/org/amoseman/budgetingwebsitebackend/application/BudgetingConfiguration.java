d2cec10fc966cb6e917f69b497ac8197f3a7392c
a-moseman <drewmoseman@gmail.com>
2024-09-09 13:15:51 -0400

    Add admin username and password to configuration

--- src/main/java/org/amoseman/budgetingwebsitebackend/application/BudgetingConfiguration.java
+++ src/main/java/org/amoseman/budgetingwebsitebackend/application/BudgetingConfiguration.java
@@ -7,9 +7,23 @@ import jakarta.validation.constraints.NotEmpty;
 public class BudgetingConfiguration extends Configuration {
     @NotEmpty
     private String databaseURL;
+    @NotEmpty
+    private String adminUsername;
+    @NotEmpty
+    private String adminPassword;
 
     @JsonProperty("database-url")
     public String getDatabaseURL() {
         return databaseURL;
+    }
+
+    @JsonProperty("admin-username")
+    public String getAdminUsername() {
+        return adminUsername;
+    }
+
+    @JsonProperty("admin-password")
+    public String getAdminPassword() {
+        return adminPassword;
     }
 }
