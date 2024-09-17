126ff4c9713317b927e15815ec70b506790d4b16
a-moseman <drewmoseman@gmail.com>
2024-09-17 13:44:57 -0400

    Add JavaDoc

--- src/main/java/org/amoseman/budgetingwebsitebackend/pojo/Entity.java
+++ src/main/java/org/amoseman/budgetingwebsitebackend/pojo/Entity.java
@@ -1,16 +1,23 @@
 package org.amoseman.budgetingwebsitebackend.pojo;
 
-import org.jooq.Record;
-import org.jooq.TableRecord;
-
 import java.beans.ConstructorProperties;
 import java.time.LocalDateTime;
 
+/**
+ * Represents a generic entity.
+ * It is the most basic form of a record in the system.
+ */
 public class Entity {
     public final String uuid;
     public final LocalDateTime created;
     public final LocalDateTime updated;
 
+    /**
+     * Instantiate an entity.
+     * @param uuid the UUID of the entity.
+     * @param created when the entity was created.
+     * @param updated when the entity was last updated.
+     */
     @ConstructorProperties({"uuid", "created", "updated"})
     public Entity(String uuid, LocalDateTime created, LocalDateTime updated) {
         this.uuid = uuid;
--- src/main/java/org/amoseman/budgetingwebsitebackend/pojo/OwnedEntity.java
+++ src/main/java/org/amoseman/budgetingwebsitebackend/pojo/OwnedEntity.java
@@ -3,9 +3,19 @@ package org.amoseman.budgetingwebsitebackend.pojo;
 import java.beans.ConstructorProperties;
 import java.time.LocalDateTime;
 
+/**
+ * Represents an owned entity.
+ */
 public class OwnedEntity extends Entity {
     public final String owner;
 
+    /**
+     * Instantiate an owned entity.
+     * @param uuid the UUID of the entity.
+     * @param created when the entity was created.
+     * @param updated when the entity was last updated.
+     * @param owner the UUID of the account that owns this entity.
+     */
     @ConstructorProperties({"uuid", "created", "updated", "owner"})
     public OwnedEntity(String uuid, LocalDateTime created, LocalDateTime updated, String owner) {
         super(uuid, created, updated);
