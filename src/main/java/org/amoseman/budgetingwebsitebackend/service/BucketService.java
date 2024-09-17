802424357bcd1417c3cac90816a4cb278b35b00c
a-moseman <drewmoseman@gmail.com>
2024-09-17 13:31:26 -0400

    Decompose BucketService.getBuckets and add JavaDoc

--- src/main/java/org/amoseman/budgetingwebsitebackend/service/BucketService.java
+++ src/main/java/org/amoseman/budgetingwebsitebackend/service/BucketService.java
@@ -13,19 +13,37 @@ import org.amoseman.budgetingwebsitebackend.pojo.bucket.op.UpdateBucket;
 import org.amoseman.budgetingwebsitebackend.util.Now;
 import org.amoseman.budgetingwebsitebackend.util.Split;
 import org.amoseman.budgetingwebsitebackend.util.Splitter;
+import org.jooq.impl.QOM;
 
 import java.time.LocalDateTime;
 import java.util.*;
 
+/**
+ * A service for bucket logic.
+ * @param <C> the client type.
+ */
 public class BucketService<C> {
     private final BucketDAO<C> bucketDAO;
     private final FinanceRecordDAO<C> financeRecordDAO;
 
+    /**
+     * Instantiate a new bucket service.
+     * @param bucketDAO the bucket data access object to use.
+     * @param financeRecordDAO the finance record data access object to use.
+     */
     public BucketService(BucketDAO<C> bucketDAO, FinanceRecordDAO<C> financeRecordDAO) {
         this.bucketDAO = bucketDAO;
         this.financeRecordDAO = financeRecordDAO;
     }
 
+    /**
+     * Add a new bucket.
+     * @param user the UUID of the user.
+     * @param create the information of the bucket.
+     * @return the UUID of the new bucket.
+     * @throws BucketAlreadyExistsException if the bucket already exists.
+     * @throws TotalBucketShareExceededException if this would result in the user's total bucket share to exceed 1.
+     */
     public String addBucket(String user, CreateBucket create) throws BucketAlreadyExistsException, TotalBucketShareExceededException {
         String uuid = UUID.randomUUID().toString();
         LocalDateTime now = Now.get();
@@ -42,10 +60,24 @@ public class BucketService<C> {
         return uuid;
     }
 
+    /**
+     * Remove a user's bucket.
+     * This does not delete the expense records associated with the bucket, but instead leaves them orphaned.
+     * @param user the UUID of the user.
+     * @param uuid the UUID of the bucket.
+     * @throws BucketDoesNotExistException if the bucket does not exist.
+     */
     public void removeBucket(String user, String uuid) throws BucketDoesNotExistException {
         bucketDAO.removeBucket(user, uuid);
     }
 
+    /**
+     * Update a user's bucket.
+     * @param user the UUID of the user.
+     * @param uuid the UUID of the bucket.
+     * @param update the update information.
+     * @throws BucketDoesNotExistException if the bucket does not exist.
+     */
     public void updateBucket(String user, String uuid, UpdateBucket update) throws BucketDoesNotExistException {
         LocalDateTime now = Now.get();
         Optional<Bucket> maybe = bucketDAO.getBucket(user, uuid);
@@ -57,9 +89,27 @@ public class BucketService<C> {
         bucketDAO.updateBucket(bucket);
     }
 
+    /**
+     * Get the user's buckets.
+     * This will calculate the total funds associated with each bucket.
+     * @param user the UUID of the user.
+     * @return the buckets.
+     */
     public List<Bucket> getBuckets(String user) {
         List<Bucket> buckets = bucketDAO.getBuckets(user);
         List<Income> incomeRecords = financeRecordDAO.getAllIncome(user);
+        List<Expense> expenseRecords = financeRecordDAO.getAllExpenses(user);
+        applyIncome(buckets, incomeRecords);
+        applyExpenses(buckets, expenseRecords);
+        return buckets;
+    }
+
+    /**
+     * Apply the income records to the amounts of the buckets.
+     * @param buckets the buckets.
+     * @param incomeRecords the income records.
+     */
+    private void applyIncome(List<Bucket> buckets, List<Income> incomeRecords) {
         incomeRecords.forEach(income -> {
             Split split = Splitter.get(buckets, income.amount);
             for (int i = 0; i < buckets.size(); i++) {
@@ -68,13 +118,19 @@ public class BucketService<C> {
                 bucket.amount += amount;
             }
         });
+    }
+
+    /**
+     * Apply the expense records to the amounts of the buckets.
+     * @param buckets the buckets.
+     * @param expenseRecords the expense records.
+     */
+    private void applyExpenses(List<Bucket> buckets, List<Expense> expenseRecords) {
         Map<String, Bucket> map = new HashMap<>();
         buckets.forEach(bucket -> map.put(bucket.uuid, bucket));
-        List<Expense> expenseRecords = financeRecordDAO.getAllExpenses(user);
         expenseRecords.forEach(expense -> {
             Bucket bucket = map.get(expense.bucket);
             bucket.amount -= expense.amount;
         });
-        return buckets;
     }
 }
