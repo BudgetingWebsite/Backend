792defc5a2636c0d94cc222d0f4627e01e41316c
a-moseman <drewmoseman@gmail.com>
2024-09-20 12:05:01 -0400

    Add javadoc

--- src/main/java/org/amoseman/budgetingbackend/exception/NegativeValueException.java
+++ src/main/java/org/amoseman/budgetingbackend/exception/NegativeValueException.java
@@ -1,6 +1,14 @@
 package org.amoseman.budgetingbackend.exception;
 
+/**
+ * An exception for when negative values occurring when they shouldn't.
+ */
 public class NegativeValueException extends Exception {
+
+    /**
+     * Instantiate an exception for the occurrence of a negative value when it shouldn't.
+     * @param value the negative value.
+     */
     public NegativeValueException(long value) {
         super(String.format("Negative value of %d occurred", value));
     }
